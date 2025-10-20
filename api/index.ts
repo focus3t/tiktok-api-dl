45476a4bdad16260b7927761781d16637d5e4072
focus3t <tananx.2023@gmail.com>
2025-10-20 23:00:54 +0700

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-import TiktokDL from '../src/index';
+import { TiktokDL } from '../src/index';
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
@@ -9,7 +9,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
       return res.status(400).json({ error: 'Thiếu tham số ?url=' });
     }
 
-    const result = await TiktokDL(url);
+    const result = await TiktokDL(url, { version: "v1" });
     res.status(200).json(result);
   } catch (error: any) {
     res.status(500).json({ error: error.message || 'Lỗi xử lý TikTok' });
